7ceb94b9a879a6111691e0b66a702f5f539ba7b5
Twinkal <tinkal@gmail.com>
2022-02-13 17:36:38 +0530

    Admin register

--- src/app/core/service/admin-api/admin/admin-auth.service.ts
+++ src/app/core/service/admin-api/admin/admin-auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from '../../base/base.service';
-
+import { map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,31 +8,32 @@ export class AdminAuthService {
 
   constructor(
     public baseService: BaseService
-  
+
   ) { }
-  
+
   login(body: any) {
-  return this.baseService.post('adminauth/login', body, false).subscribe(
-    (data: any) => {
-      if (data.status === 200) {
-        localStorage.setItem('token', data.data.token);
+    return this.baseService.post('adminauth/login', body, false).subscribe(
+      (data: any) => {
+        if (data.status === 200) {
+          localStorage.setItem('admintoken', data.data.token);
+        }
+        return data;
+      },
+      (error: any) => {
+        return error;
       }
-      return data;
-    },
-    (error: any) => {
-      return error;
-    }
-  ); 
+    );
   }
 
-  register(body: any) {
-   return this.baseService.post('adminauth/register', body, false).subscribe(
-     (data: any) => {
-       return data;
-     },
-     (error: any) => {
-       return error;
-     }
-   );
+  public register(body: any) {
+    return this.baseService.post('adminauth/register', body, false).pipe(map<any, any>(
+      (response: any) => {
+        return response;
+      },
+      (error: any) => {
+        return error;
+      }
+    )
+    );
   }
 }
--- src/app/user/welcome/welcome.component.ts
+++ src/app/user/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/service/auth/auth.service';
 
 @Component({
@@ -8,10 +9,14 @@ import { AuthService } from 'src/app/core/service/auth/auth.service';
 })
 export class WelcomeComponent implements OnInit {
   userModel: any;
-  constructor(private authService: AuthService,) { }
+  constructor(private authService: AuthService,
+    private router: Router,) { }
 
   ngOnInit(): void {
     this.userModel = this.authService.getUserProfile();
   }
-
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/']);
+  }
 }
